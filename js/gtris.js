f0be7fd5b1af34593e1c1dbf73f2b275f818da47
DESKTOP-DAB6NIR\kimdy <kimdy@gabia.com>
2017-08-09 16:43:41 +0900

    dropdown add

--- js/gtris.js
+++ js/gtris.js
@@ -461,3 +461,44 @@
 	gtris.ui.tab = tab;
 
 })(window.gtris);
+
+(function(gtris) {
+	'use strict';
+	if (!gtris) {
+		gtris = window.gtris = {};
+	}
+	if (!gtris.ui) {
+		gtris.ui = window.gtris.ui = {};
+	}
+
+	var dropdown = {
+
+		init: function(obj) {
+			var $dd = $(obj.target);
+			this.addEvent($dd);
+		},
+
+		toggleDD: function(event) {
+			var $dd_button = $(event.target);
+			$dd_button.parents('.gt-dropdown').toggleClass('gt-active');
+		},
+
+		documentClick: function(event) {
+			var $dd_button = $(event.target);
+			if( $dd_button.is('.gt-button') || $dd_button.is('.gt-btn') === false ) {
+				var dropdownMenu = $('.gt-dd-menu');
+				dropdownMenu.parents('.gt-dropdown').removeClass('gt-active');
+			}
+		},
+
+		addEvent: function($dd) {
+			$dd.find('.gt-button').on('click', this.toggleDD);
+			$dd.find('.gt-btn').on('click', this.toggleDD);
+			$(document).on('click', this.documentClick);
+		}
+
+	};
+
+	gtris.ui.dropdown = dropdown;
+
+})(window.gtris);
