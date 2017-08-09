0fdc1ceb22393cf9d5674aa02d1b3c3da881aa8c
DESKTOP-DAB6NIR\kimdy <kimdy@gabia.com>
2017-08-09 14:22:04 +0900

    tab 추가

--- js/gtris.js
+++ js/gtris.js
@@ -53,6 +53,31 @@
 })(window.gtris);
 
 (function(gtris) {
+    'use strict';
+    if (!gtris) {
+        gtris = window.gtris = {};
+    }
+    if (!gtris.util) {
+        gtris.util = window.gtris.util = {};
+    }
+
+    var getParameterByName = function(name) {
+        if (name === undefined || name === '') return;
+        var query = window.location.search.substring(1);
+        var vars = query.split('&');
+        for (var i=0; i<vars.length; i++) {
+            var pair = vars[i].split('=');
+            if(pair[0] == name) {
+                return pair[1];
+            }
+        }
+    };
+
+    gtris.util.getParameterByName = getParameterByName;
+
+})(window.gtris);
+
+(function(gtris) {
 	'use strict';
 	if (!gtris) {
 		gtris = window.gtris = {};
@@ -362,5 +387,77 @@
 	};
 
 	gtris.ui.tooltip = tooltip;
+
+})(window.gtris);
+
+(function(gtris) {
+	'use strict';
+	if (!gtris) {
+		gtris = window.gtris = {};
+	}
+	if (!gtris.ui) {
+		gtris.ui = window.gtris.ui = {};
+	}
+	var tab = {
+		init: function(obj) {
+			var _obj = obj;
+			this.attachTabEvent(_obj);		
+		},
+		getParam: function(paramName) {
+			var val = gtris.util.getParameterByName(paramName);
+			return val;
+		},
+		attachTabEvent: function(_obj) {			
+			var $target = $(_obj.target);
+			var idx = Number(this.getParam('idx'));
+			if(idx < 0 || idx > _obj.target.length-1 || window.isNaN(idx)) {
+				idx = 0;
+			}
+			
+			if(!_obj.event) {
+				_obj.event = 'click';
+			}
+			
+			$target.each(function() {
+				var $tab_head = $(this);
+				$tab_head.target_id = [];
+
+				$tab_head.find('[data-id]').each(function() {
+					var $tab_nav = $(this);
+					var this_id = "#" + $(this).attr('data-id');
+					$tab_head.target_id.push(this_id);
+					$tab_nav.on(_obj.event, function() {
+						tab.executeTabEvent.call(this, $tab_head, $(this).index());
+					});
+				});
+				$tab_head.find('.gt-nav-item').eq(idx).find('[data-id]').trigger(_obj.event);
+			});
+		},
+		ajaxCall: function(this_id) {
+			$.ajax({
+				url: $(this).attr('data-url'),
+				beforeSend : function() {
+					$(this_id).empty().append('<div style="text-align:center;padding:10px 0;"><img src="https://static.gabia.com/gtris/assets/images/gt-loader.gif"></div>');
+				}
+			}).done(function(response) {
+				$(this_id).empty().append(response);
+			}).fail(function(jqXHR, textStatus, errorThrown) {
+				window.alert('load failed.');
+				$(this_id).empty().append('jqXHR: ' + jqXHR + ', textStatus: ' + textStatus + ', errorThrown: ' + errorThrown);
+			});
+		},
+		executeTabEvent: function($tab_head, idx) {			
+			var this_id = "#" + $(this).attr('data-id');			
+			if((/(http(s)?:\/)?(\/\w+)+(\.[\w.]+)?/g).test($(this).attr('data-url'))) {
+				tab.ajaxCall.call(this, this_id);
+			}			
+			
+			$tab_head.find(".gt-nav-item.gt-active").removeClass("gt-active");
+			$tab_head.find(".gt-nav-item").eq(idx).addClass("gt-active");
+			$($tab_head.target_id.join(", ")).hide();			
+			$(this_id).show();
+		}
+	};
+	gtris.ui.tab = tab;
 
 })(window.gtris);
