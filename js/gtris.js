8bffbce693f14d360694af7b2282ad8091c42810
DESKTOP-DAB6NIR\kimdy <kimdy@gabia.com>
2017-08-11 17:52:41 +0900

    util 추가

--- js/gtris.js
+++ js/gtris.js
@@ -1,4 +1,280 @@
 (function(gtris) {
+    'use strict';
+    if (!gtris) {
+        gtris = window.gtris = {};
+    }
+    if (!gtris.util) {
+        gtris.util = window.gtris.util = {};
+    }
+
+    var addComma = function(num) {
+        var _num = String(num).replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+		return _num;
+    };
+    
+    gtris.util.addComma = addComma;
+
+})(window.gtris);
+
+(function(gtris) {
+    'use strict';
+    if (!gtris) {
+        gtris = window.gtris = {};
+    }
+    if (!gtris.util) {
+        gtris.util = window.gtris.util = {};
+    }
+
+    var autoDashDate = function(str) {
+        var _str = str.replace(/[^0-9]/g, '');
+        var tmp = '';
+        if(_str.length < 5) {
+           return str;
+        }else if(_str.length < 7) {
+            tmp += _str.substr(0, 4);
+            tmp += '-';
+            tmp += _str.substr(4, 5);
+            return tmp;
+        }else{
+            tmp += _str.substr(0, 4);
+            tmp += '-';
+            tmp += _str.substr(4, 2);
+            tmp += '-';
+            tmp += _str.substr(6, 2);
+            return tmp;
+        }
+    };
+
+    gtris.util.autoDashDate = autoDashDate;
+
+})(window.gtris);
+
+(function(gtris) {
+	'use strict';
+	if (!gtris) {
+		gtris = window.gtris = {};
+	}
+	if (!gtris.util) {
+		gtris.util = window.gtris.util = {};
+	}
+
+	var autoDashPhoneNumber = function(str) {
+		var _str = str.replace(/[^0-9]/g, '');
+		var tmp = '';
+		if(_str.length < 4) {
+			return _str;
+		}else if(_str.length < 7) {
+			tmp += _str.substr(0, 3);
+			tmp += '-';
+			tmp += _str.substr(3);
+			return tmp;
+		}else if(_str.length < 11) {
+			tmp += _str.substr(0, 3);
+			tmp += '-';
+			tmp += _str.substr(3, 3);
+			tmp += '-';
+			tmp += _str.substr(6);
+			return tmp;
+		}else{
+			tmp += _str.substr(0, 3);
+			tmp += '-';
+			tmp += _str.substr(3, 4);
+			tmp += '-';
+			tmp += _str.substr(7);
+			return tmp;
+		}
+		return str;
+	};
+
+	gtris.util.autoDashPhoneNumber = autoDashPhoneNumber;
+
+})(window.gtris);
+
+(function(gtris) {
+    'use strict';
+    if (!gtris) {
+        gtris = window.gtris = {};
+    }
+    if (!gtris.util) {
+        gtris.util = window.gtris.util = {};
+    }
+
+    var browser = function() {
+        
+		var browserPool = [
+			{detectStr: "MSIE", name: "ie"},
+			{detectStr: "Trident", name: "ie"},
+			{detectStr: "Edge", name: "edge"},
+			{detectStr: "Firefox", name: "firefox"},
+			{detectStr: "OPR", name: "opera"},
+			{detectStr: "Opera mini", name: "opera mini"},
+			{detectStr: "Chrome", name: "chrome"},
+			{detectStr: "CriOS", name: "chrome"},
+			{detectStr: "Safari", name: "safari"}
+		];
+
+        var ua = navigator.userAgent;
+        var n, v, vOffset;
+		for(var i=0; i<browserPool.length; i++) {
+			if((vOffset = ua.indexOf(browserPool[i].detectStr)) > -1) {
+				n = browserPool[i].name;
+				if(browserPool[i].detectStr == 'Trident') { //ie11
+					v = ua.substr(ua.indexOf('rv:') + 3);
+					v = parseFloat(v.split(')')[0]);
+				}
+				else if(browserPool[i].detectStr == 'Safari') {
+					v = ua.substr(ua.indexOf('Version/') + 8);
+					v = parseFloat(v.split('/')[0]);
+				}
+				else {
+					v = ua.substr(vOffset + browserPool[i].detectStr.length + 1);
+					v = parseFloat(v.split(' ')[0]);
+				}
+				break;
+			}else{
+				n = "unknown";
+				v = -1;
+			}
+		}
+        return {name: n, version: v};
+    };
+
+    gtris.util.browser = browser();
+
+})(window.gtris);
+
+(function(gtris) {
+	'use strict';
+	if (!gtris) {
+		gtris = window.gtris = {};
+	}
+	if (!gtris.util) {
+		gtris.util = window.gtris.util = {};
+	}
+	
+	var setCookie = function(cname, cvalue, exdays) {
+		var d = new Date();
+		d.setTime(d.getTime() + (exdays*24*60*60*1000));
+		var expires = "expires=" + d.toGMTString();
+		document.cookie = cname + "=" + cvalue + "; " + expires;
+	};
+	
+	var getCookie = function(cname) {
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0; i<ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ') {
+				c = c.substring(1);
+			}
+			if (c.indexOf(name) != -1) {
+				return c.substring(name.length, c.length);
+			}
+		}
+		return "";
+	};
+	
+	gtris.util.setCookie = setCookie;
+	gtris.util.getCookie = getCookie;
+
+})(window.gtris);
+(function(gtris) {
+    'use strict';
+    if (!gtris) {
+        gtris = window.gtris = {};
+    }
+    if (!gtris.util) {
+        gtris.util = window.gtris.util = {};
+    }
+
+    var copyToClipboard = function(text) {
+        if(text === undefined || text === '') return;
+        var _txt = text;
+        if(window.clipboardData) {
+            window.clipboardData.setData("text", _txt);
+        }else{
+            var textarea = document.createElement("textarea");
+            textarea.textContent = _txt;
+            textarea.style.position = "fixed";
+            document.body.appendChild(textarea);
+            textarea.select();
+            try {
+                return document.execCommand("copy");
+            } catch (ex) {
+                return false;
+            } finally {
+                document.body.removeChild(textarea);
+            }
+        }
+    };
+
+    gtris.util.copyToClipboard = copyToClipboard;
+
+})(window.gtris);
+
+(function(gtris) {
+    'use strict';
+    if (!gtris) {
+        gtris = window.gtris = {};
+    }
+    if (!gtris.util) {
+        gtris.util = window.gtris.util = {};
+    }
+
+    var getParameterByName = function(name) {
+        if (name === undefined || name === '') return;
+        var query = window.location.search.substring(1);
+        var vars = query.split('&');
+        for (var i=0; i<vars.length; i++) {
+            var pair = vars[i].split('=');
+            if(pair[0] == name) {
+                return pair[1];
+            }
+        }
+    };
+
+    gtris.util.getParameterByName = getParameterByName;
+
+})(window.gtris);
+
+(function(gtris) {
+    'use strict';
+    if (!gtris) {
+        gtris = window.gtris = {};
+    }
+    if (!gtris.util) {
+        gtris.util = window.gtris.util = {};
+    }
+
+    var isMobile = function() {
+		var ua = navigator.userAgent;
+		var detectStr = /Android|Mobile|iP(hone|od|ad)|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/;
+		return detectStr.test(ua);
+    };
+
+    gtris.util.isMobile = isMobile();
+
+})(window.gtris);
+
+(function(gtris) {
+    'use strict';
+    if (!gtris) {
+        gtris = window.gtris = {};
+    }
+    if (!gtris.util) {
+        gtris.util = window.gtris.util = {};
+    }
+
+    var isValidateEmail = function(email) {
+        var reg_email = /^[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[@]{1}[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[.]{1}[A-Za-z]{2,5}$/;
+        return reg_email.test(email);
+    };
+
+    gtris.util.isValidateEmail = isValidateEmail;
+
+})(window.gtris);
+
+(function(gtris) {
 	'use strict';
 	if (!gtris) {
 		gtris = window.gtris = {};
@@ -53,6 +329,81 @@
 })(window.gtris);
 
 (function(gtris) {
+	'use strict';
+	if (!gtris) {
+		gtris = window.gtris = {};
+	}
+	if (!gtris.util) {
+		gtris.util = window.gtris.util = {};
+	}
+
+	var os = function() {
+		var ua = navigator.userAgent;
+		var aver = navigator.appVersion;
+		var osPool = [
+			{name:"Windows 10", detectStr:/(Windows 10.0|Windows NT 10.0)/},
+			{name:"Windows 8.1", detectStr:/(Windows 8.1|Windows NT 6.3)/},
+			{name:"Windows 8", detectStr:/(Windows 8|Windows NT 6.2)/},
+			{name:"Windows 7", detectStr:/(Windows 7|Windows NT 6.1)/},
+			{name:"Windows Vista", detectStr:/Windows NT 6.0/},
+			{name:"Windows Server 2003", detectStr:/Windows NT 5.2/},
+			{name:"Windows XP", detectStr:/(Windows NT 5.1|Windows XP)/},
+			{name:"Windows 2000", detectStr:/(Windows NT 5.0|Windows 2000)/},
+			{name:"Windows ME", detectStr:/(Win 9x 4.90|Windows ME)/},
+			{name:"Windows 98", detectStr:/(Windows 98|Win98)/},
+			{name:"Windows 95", detectStr:/(Windows 95|Win95|Windows_95)/},
+			{name:"Windows NT 4.0", detectStr:/(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/},
+			{name:"Windows CE", detectStr:/Windows CE/},
+			{name:"Windows 3.11", detectStr:/Win16/},
+			{name:"Android", detectStr:/Android/},
+			{name:"Open BSD", detectStr:/OpenBSD/},
+			{name:"Sun OS", detectStr:/SunOS/},
+			{name:"Linux", detectStr:/(Linux|X11)/},
+			{name:"iOS", detectStr:/iP(hone|od|ad)/},
+			{name:"Mac OS X", detectStr:/Mac OS X/},
+			{name:"Mac OS", detectStr:/Mac(PPC|Intel|_PowerPC|intosh)/},
+			{name:"QNX", detectStr:/QNX/},
+			{name:"UNIX", detectStr:/UNIX/},
+			{name:"BeOS", detectStr:/BeOS/},
+			{name:"OS/2", detectStr:/OS\/2/},
+			{name:"Search Bot", detectStr:/(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/}
+		];
+		var n, v;
+
+		for(var i=0; i<osPool.length; i++) {
+			if(osPool[i].detectStr.test(ua)) {
+				n = osPool[i].name;
+				break;
+			}else {
+				n = "unknown";
+			}
+		}
+		switch (n) {
+			case 'Mac OS X':
+				v = /Mac OS X (10[\.\_\d]+)/.exec(ua)[1];
+				break;
+			case 'Android':
+				v = /Android ([\.\_\d]+)/.exec(ua)[1];
+				break;
+			case 'iOS':
+				v = /OS (\d+)_(\d+)_?(\d+)?/.exec(aver);
+				v = v[1] + '.' + v[2] + '.' + (v[3] | 0);
+				break;
+			default:
+				v = -1;
+		}
+		if(/Windows/.test(n)) {
+			v = /Windows (.*)/.exec(n)[1];
+			n = 'Windows';
+		}
+		return {name: n, version: v};
+	};
+
+	gtris.util.os = os();
+
+})(window.gtris);
+
+(function(gtris) {
     'use strict';
     if (!gtris) {
         gtris = window.gtris = {};
@@ -61,19 +412,12 @@
         gtris.util = window.gtris.util = {};
     }
 
-    var getParameterByName = function(name) {
-        if (name === undefined || name === '') return;
-        var query = window.location.search.substring(1);
-        var vars = query.split('&');
-        for (var i=0; i<vars.length; i++) {
-            var pair = vars[i].split('=');
-            if(pair[0] == name) {
-                return pair[1];
-            }
-        }
+    var removeComma = function(num) {
+        var str = Number(num.replace(/\,/g, ''));
+        return str;
     };
 
-    gtris.util.getParameterByName = getParameterByName;
+    gtris.util.removeComma = removeComma;
 
 })(window.gtris);
 
@@ -479,7 +823,9 @@
 		},
 
 		toggleDD: function(event) {
+			event.stopPropagation();
 			var $dd_button = $(event.target);
+			console.log($dd_button)
 			$dd_button.parents('.gt-dropdown').toggleClass('gt-active');
 		},
 
@@ -500,5 +846,102 @@
 	};
 
 	gtris.ui.dropdown = dropdown;
+
+})(window.gtris);
+
+(function(gtris) {
+	'use strict';
+	if (!gtris) {
+		gtris = window.gtris = {};
+	}
+	if (!gtris.ui) {
+		gtris.ui = window.gtris.ui = {};
+	}
+
+	var toast = {
+
+		$toastContainer : '<div>',
+
+		open: function(options) {
+			var $toast;
+
+			if( $('.gt-toast-container').length === 0 ) {
+				this.$toastContainer = $(document.createElement('div'));
+				this.$toastContainer.addClass('gt-toast-container');
+				this.$toastContainer.appendTo('body');
+
+
+				if(options.position === undefined || options.position ==='') {
+					options.position = 'top-right';
+				}
+
+				this.$toastContainer.attr('data-placement', options.position);
+
+			}
+
+			$toast = $(document.createElement('div'));
+			$toast.addClass('gt-toast');
+			$toast.appendTo(this.$toastContainer);
+
+			$toast.animate({opacity: 0}, 0);
+			$toast.animate({opacity: 0.8}, 500);
+			
+			console.log( "<code>" + options.message + "</code>" )
+
+			$toast.html( options.message );
+
+
+
+
+			switch(options.type) {
+				case 'primary':
+				$toast.addClass('gt-toast-primary');
+				break;
+
+				case 'success':
+				$toast.addClass('gt-toast-success');
+				break;
+
+				case 'info':
+				$toast.addClass('gt-toast-info');
+				break;
+
+				case 'warning':
+				$toast.addClass('gt-toast-warning');
+				break;
+
+				case 'danger':
+				$toast.addClass('gt-toast-danger');
+				break;
+			}
+
+			$toast.on('click', this.deleteToast);
+			$toast.on('mouseover', this.mouseoverToast);
+
+			$toast.mouseover(function(event) {
+				$(this).css('opacity', '1');
+			});
+
+			$toast.mouseout(function(event) {
+				$(this).css('opacity', '0.8');
+			});
+
+		},
+
+		deleteToast: function(event) {
+			if( $('.gt-toast-container .gt-toast').length === 1 ) {
+				$('.gt-toast-container').remove();
+			}
+
+			$(this).remove();
+		},
+
+		mouseoverToast: function() {
+			
+		}
+
+	};
+
+	gtris.ui.toast = toast;
 
 })(window.gtris);
