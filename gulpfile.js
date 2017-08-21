26f0eb034c7126d2b1890c2ab26b2a7430df167c
Nahui Jang <jnh@gabia.com>
2017-08-21 11:05:46 +0900

    버튼 개선

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps');
+
+// paths
+var DIR = {
+	MAP: 'map'
+};
+
+/* variables */
+var paths = {
+	src: {
+		gt_scss: 'css/**/*.scss',
+	},
+	dist: {
+		gt_css: 'css/',
+	}
+}
+
+// task
+gulp.task('sass', function () {
+	return gulp.src(paths.src.gt_scss)
+	.pipe(sourcemaps.init())
+	.pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
+	.pipe(sourcemaps.write(DIR.MAP))
+		.pipe(gulp.dest( paths.dist.gt_css ));
+});
+
+// watch
+gulp.task('watch', function () {
+	gulp.watch( paths.src.gt_scss, ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
