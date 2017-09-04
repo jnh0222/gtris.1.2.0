46e3bc0ab1c07a95aeacef36715b71d5790e168c
DESKTOP-DAB6NIR\kimdy <kimdy@gabia.com>
2017-09-04 16:08:02 +0900

    scrollprogress edit

--- js/gtris.js
+++ js/gtris.js
@@ -959,37 +959,30 @@
 		progress_bar: 'progress_bar',
 
 		init: function(obj) {
-			if(gtris.util.browser.name === 'ie' && gtris.util.browser.version <= 9) {
-				return;
-			}else{
-				this.progress_bar = $(obj.target);
-				this.progress_bar.attr({ max: this.getMax() });
-				this.addEvent(this.progress_bar);
-				this.windowResize();
-			}
+			if(!obj.height) obj.height = 5;
+			if(!obj.bgColor) obj.bgColor = '#2598e7';
+			this.progress_bar = $(obj.target);
+			this.progress_bar.css('height', obj.height);
+			this.progress_bar.css('background-color', obj.bgColor);
+			this.addEvent(this.progress_bar);
+			this.updateProgress();
 		},
 
-		documentScroll: function(event) {
-			scrollprogress.progress_bar.attr({ value: scrollprogress.getValue() });
+		updateProgress: function(event) {
+			var scroll_top = $(window).scrollTop(); //current vertical position of the scroll bar
+			var bottom_page = $(document).height() - $(window).height(); //(height of HTML document - height of browser viewport)
+			scrollprogress.progress_bar.css('width',  scrollprogress.calculatePercent(scroll_top, bottom_page) );
 		},
 
-		windowResize: function(event) {
-			scrollprogress.progress_bar.attr({ max: scrollprogress.getMax(), value: scrollprogress.getValue() });
-		},
-
-		getMax: function() {
-			return $(document).height() - $(window).height();
-		},
-
-		getValue: function() {
-			return $(window).scrollTop();
+		calculatePercent: function(num1, num2) {
+			var percent = Math.ceil( num1 / num2 * 100 ) + '%';
+			return percent;
 		},
 
 		addEvent: function(progress_bar) {
-			$(document).on('scroll', this.documentScroll);
-			$(window).on('resize', this.windowResize);
+			$(document).on('scroll', this.updateProgress);
+			$(window).on('resize', this.updateProgress);
 		}
-
 	};
 
 	gtris.ui.scrollprogress = scrollprogress;
