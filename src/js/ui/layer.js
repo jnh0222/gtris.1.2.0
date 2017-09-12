a2410e12d4ea6ea4faa9674517672921f33c271c
DESKTOP-DAB6NIR\kimdy <kimdy@gabia.com>
2017-09-12 17:08:33 +0900

    레이어 창 닫을 시에 포커스 자동 이동 기능

--- src/js/ui/layer.js
+++ src/js/ui/layer.js
@@ -53,6 +53,7 @@
 						$ly_container.find('[data-layer="hide"]').on('click', function(event) {
 							event.stopPropagation();
 							this.isHasLayer = false;
+							focusedElementBeforeWindow.focus();
 							$ly_container.remove();
 							if(obj.closed) return obj.closed();
 						});
